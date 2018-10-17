83053dcbd7b615d7310a46fe03fc37c0527efc13
Jan <janhajk@gmail.com>
2018-10-17 09:45:07 +0000

    bug

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ var api_call_meter = function(port, host, call, cb) {
    });
 };
 
+var get_PAC = function(cb) {
+   var call = '/solar_api/v1/GetInverterRealtimeData.cgi?Scope=System';
+   api_call_meter(config.meter.port, config.meter.host, call, function(err, response, body) {
+      console.log('> IP ' + config.meter.host + '...');
+      var pac = body.body.Data.PAC.Values.1;
+      cb(pac);
+   });
+};
+
 
 api_call_miner(config.miner.port, config.miner.host, '{"id":2,"jsonrpc":"2.0","method":"miner_getstat1"}', function(r){
    console.log(r.toString());
@@ -37,4 +46,8 @@ api_call_miner(config.miner.port, config.miner.host, '{"id":2,"jsonrpc":"2.0","m
 api_call_meter(config.meter.port, config.meter.host, '/solar_api/v1/GetInverterRealtimeData.cgi?Scope=System', function(err, response, body){
    console.log('IP ' + config.meter.host);
    console.log(body);
+});
+
+get_PAC(function(pac){
+   console.log('PAC: ' + pac + ' Watt');
 });
