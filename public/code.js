6f80dccfd195f715fa382dae37471d3c8b67dd44
janhajk <janhajk@gmail.com>
2018-11-23 12:43:13 +0000

    website

--- public/code.js
+++ public/code.js
@@ -1,64 +1,19 @@
-Highcharts.chart('container', {
-    chart: {
-        zoomType: 'x'
-    },
-    title: {
-        text: 'Strom'
-    },
-    subtitle: {
-        text: document.ontouchstart === undefined ?
-            'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-    },
-    data: {
+function createChart() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Strom'
+        },
+        data: {
             rowsURL: 'https://solarweb.servus.janschaer.ch/data/json',
             enablePolling: true,
-            dataRefreshRate: 5
-        
-    },
-    xAxis: {
-        type: 'datetime'
-    },
-    yAxis: {
-        title: {
-            text: 'kW'
+            dataRefreshRate: 2
         }
-    },
-    legend: {
-        enabled: false
-    },
-    plotOptions: {
-        area: {
-            fillColor: {
-                linearGradient: {
-                    x1: 0,
-                    y1: 0,
-                    x2: 0,
-                    y2: 1
-                },
-                stops: [
-                    [0, Highcharts.getOptions().colors[0]],
-                    [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                ]
-            },
-            marker: {
-                radius: 2
-            },
-            lineWidth: 1,
-            states: {
-                hover: {
-                    lineWidth: 1
-                }
-            },
-            threshold: null
-        }
-    },
-    series: [{
-            type: 'area',
-            name: 'Stromproduktion',
-        },
-        {
-            type: 'area',
-            name: 'Stromverbrauch',
-        }
-    ]
-});
+    });
+}
+
+
+
+createChart();
